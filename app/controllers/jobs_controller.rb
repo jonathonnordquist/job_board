a13c4df20fcc727c61fe14a954e79d2771c11d74
Jonathon <jonathon.nordquist@gmail.com>
2015-11-30 23:09:09 +0000

    Create jobs controller with views for new, show and index

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,10 @@
+class JobsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root 'general_pages#home'
   
   get 'help'                            => 'general_pages#help'
+  
+  get 'jobs/index'
+  get 'jobs/new'
+  get 'jobs/show'
 
 end
