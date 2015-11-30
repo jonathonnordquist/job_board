db4ec558eb7c53980fabe31cea5ff5b2ec2edf49
Jonathon <jonathon.nordquist@gmail.com>
2015-11-30 22:18:27 +0000

    Add formatted_title helper, use provide to use helper to customize page titles.

--- app/helpers/general_pages_helper.rb
+++ app/helpers/general_pages_helper.rb
@@ -1,2 +1,10 @@
 module GeneralPagesHelper
+  def formatted_title(title = "")
+    title_base = "Rails Job Board"
+    if title.empty?
+      title_base
+    else
+      "#{title} | #{title_base}"
+    end
+  end
 end
