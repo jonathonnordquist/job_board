9c530e39de8715e0446d01deec870b776707377d
Jonathon <jonathon.nordquist@gmail.com>
2015-11-30 23:57:03 +0000

    Add handling of unsuccessful creation to jobs controller create method.  Add display of params in devel and errors on unsuccessful creation.  Add styling to errors.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,9 +7,20 @@ class JobsController < ApplicationController
   end
   
   def create
+    @job = Job.new(job_params)
     
+    if @job.save
+      
+    else
+      render 'new'
+    end
   end
 
   def show
   end
+  
+  private
+    def job_params
+      params.require(:job).permit(:company, :title, :description, :category)
+    end
 end
